590b5e055979e46fd1bb4605e452a8f6910be7a9
Alumno <Alumno@LAB1509-22>
2023-12-01 17:44:20 -0500

    Agregar archivos de proyecto.

---
+++ TaskMVVC/TaskMVVC/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using TaskMVVC.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TaskMVVC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views. Task();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
